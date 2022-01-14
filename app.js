6360b8844154303769a3da450e2e175674307140
Jorge E Gomez <jeg1986@gmail.com>
2022-01-14 12:53:49 -0300

    Header base.

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      iniciales: "JEG",
+      cargo: "Frontend Developer",
+      twitter: "https://twitter.com/jeg1986"
+    };
+  }
+}).mount("#app");
